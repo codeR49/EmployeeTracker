2bd894dc2592af3c2573753f4e26deb5bcaf9b88
m-ksingh <mstict16@gmail.com>
2022-02-25 17:12:59 +0530

    first excel upload

--- Client/src/pages/TrackerExcell/TrackerExcell.js
+++ Client/src/pages/TrackerExcell/TrackerExcell.js
@@ -1,82 +1,129 @@
-import React, { useState } from 'react';
-import MaterialTable from 'material-table'
- import * as XLSX from 'xlsx'
+import React, { Component } from 'react';
+
+import {OutTable, ExcelRenderer} from 'react-excel-renderer';
+import {  Col, Input, InputGroup, FormGroup, Label, Button, Fade, FormFeedback, Container, Card } from 'reactstrap';
+
+
  import "../EmployeeList/EmployeeList.css"
- import {OutTable, ExcelRenderer} from 'react-excel-renderer';
 
-
-const EXTENSIONS = ['xlsx', 'xls', 'csv']
-function TrackerExcell() {
-  const [colDefs, setColDefs] = useState()
-  const [data, setData] = useState()
-
-  const getExention = (file) => {
-    const parts = file.name.split('.')
-    const extension = parts[parts.length - 1]
-    return EXTENSIONS.includes(extension) // return boolean
+ class TrackerExcell extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      isOpen: false,
+      dataLoaded: false,
+      isFormInvalid: false,
+      rows: null,
+      cols: null
+    }
+    this.fileHandler = this.fileHandler.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.openFileBrowser = this.openFileBrowser.bind(this);
+    this.renderFile = this.renderFile.bind(this);
+    this.openNewPage = this.openNewPage.bind(this);
+    this.fileInput = React.createRef();
   }
 
-  const convertToJson = (headers, data) => {
-    const rows = []
-    data.forEach(row => {
-      let rowData = {}
-      row.forEach((element, index) => {
-        rowData[headers[index]] = element
-      })
-      rows.push(rowData)
+  renderFile = (fileObj) => {
+      //just pass the fileObj as parameter
+      ExcelRenderer(fileObj, (err, resp) => {
+        if(err){
+          console.log(err);            
+        }
+        else{
+          this.setState({
+            dataLoaded: true,
+            cols: resp.cols,
+            rows: resp.rows
+          });
+        }
+      }); 
+  }
 
+  fileHandler = (event) => {    
+    if(event.target.files.length){
+      let fileObj = event.target.files[0];
+      let fileName = fileObj.name;
+
+      
+      //check for file extension and pass only if it is .xlsx and display error message otherwise
+      if(fileName.slice(fileName.lastIndexOf('.')+1) === "xlsx"){
+        this.setState({
+          uploadedFileName: fileName,
+          isFormInvalid: false
+        });
+        this.renderFile(fileObj)
+      }    
+      else{
+        this.setState({
+          isFormInvalid: true,
+          uploadedFileName: ""
+        })
+      }
+    }               
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
     });
-    return rows
   }
 
-  const importExcel = (e) => {
-    const file = e.target.files[0]
-
-    const reader = new FileReader()
-    reader.onload = (event) => {
-      //parse data
-
-      const bstr = event.target.result
-      const workBook = XLSX.read(bstr, { type: "binary" })
-
-      //get first sheet
-      const workSheetName = workBook.SheetNames[0]
-      const workSheet = workBook.Sheets[workSheetName]
-      //convert to array
-      const fileData = XLSX.utils.sheet_to_json(workSheet, { header: 1 })
-      // console.log(fileData)
-      const headers = fileData[0]
-      const heads = headers.map(head => ({ title: head, field: head }))
-      setColDefs(heads)
-
-      //removing header
-      fileData.splice(0, 1)
-
-
-      setData(convertToJson(headers, fileData))
-    }
-
-    if (file) {
-      if (getExention(file)) {
-        reader.readAsBinaryString(file)
-      }
-      else {
-        alert("Invalid file input, Select Excel, CSV file")
-      }
-    } else {
-      setData([])
-      setColDefs([])
-    }
+  openFileBrowser = () => {
+    this.fileInput.current.click();
   }
 
-  return (
-    <div className="userList">
-      {/* <h1 align="center">React-App</h1>
-      <h4 align='center'>Import Data from Excel, CSV in Material Table</h4>*/}
-       <input type="file" onChange={importExcel} /> 
-      <MaterialTable title="Upload Excel" data={data} columns={colDefs} />
-    </div>
-  );
+  openNewPage = (chosenItem) => {
+    const url = chosenItem === "github" ? "https://github.com/ashishd751/react-excel-renderer" : "https://medium.com/@ashishd751/render-and-display-excel-sheets-on-webpage-using-react-js-af785a5db6a7";
+    window.open(url, '_blank');
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+                
+              <h1 className="display-3">react-excel-renderer</h1>
+              <p className="lead">Welcome to the demo of react-excel-renderer.</p>  
+              <Button className="primary jumbotron-button" onClick={this.openNewPage.bind(this,"github")}>GitHub</Button>{' '}
+              <Button className="primary jumbotron-button" onClick={this.openNewPage.bind(this,"medium")}>Medium</Button>                      
+              <hr className="my-2" />
+              <p>Developed with <span className="fa fa-heart"></span> by Ashish Deshpande</p>
+         
+        </div>
+        <Container>
+        <form className='userList'>
+          <FormGroup row>
+            <Label for="exampleFile" xs={6} sm={4} lg={2} size="lg">Upload</Label>          
+            <Col xs={4} sm={8} lg={10}>                                                     
+              <InputGroup>
+               
+                  <Button color="info" style={{color: "white", zIndex: 0}} onClick={this.openFileBrowser.bind(this)}><i className="cui-file"></i> Browse&hellip;</Button>
+                  <input type="file" hidden onChange={this.fileHandler.bind(this)} ref={this.fileInput} onClick={(event)=> { event.target.value = null }} style={{"padding":"10px"}} />                                
+              
+                <Input type="text" className="form-control" value={this.state.uploadedFileName} readOnly invalid={this.state.isFormInvalid} />                                              
+                <FormFeedback>    
+                  <Fade in={this.state.isFormInvalid} tag="h6" style={{fontStyle: "italic"}}>
+                    Please select a .xlsx file only !
+                  </Fade>                                                                
+                </FormFeedback>
+              </InputGroup>     
+            </Col>                                                   
+          </FormGroup>   
+        </form>
+
+        {this.state.dataLoaded && 
+        <div>
+          <Card body outline color="secondary" className="restrict-card">
+            
+              <OutTable data={this.state.rows} columns={this.state.cols} tableClassName="ExcelTable2007" tableHeaderRowClass="heading" />
+            
+          </Card>  
+        </div>}
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default TrackerExcell;
