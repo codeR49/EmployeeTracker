e6015ef7c1f0eabcf15fc84a2caf16e8b20e9a55
m-ksingh <mstict16@gmail.com>
2022-03-23 13:19:34 +0530

    review ui

--- Client/src/pages/Admin/ReviewEmployee/ReviewEmployee.js
+++ Client/src/pages/Admin/ReviewEmployee/ReviewEmployee.js
@@ -1,24 +1,61 @@
 import React, { useState, useEffect, useMemo, useRef } from "react";
+
+import "../../EmployeeList/EmployeeList.css"
+
 import { DataGrid, GridToolbar, GridActionsCellItem } from '@mui/x-data-grid';
 import Box from '@mui/material/Box';
-import DevelopmentUrl from "../../data/api";
-import "./EmployeeList.css"
+
+import DevelopmentUrl from "../../../data/api"
+
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import axios from "axios";
 
 import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { TextareaAutosize } from "@material-ui/core";
 
 
-
-export default function DataTable() {
+export default function ReviewEmployee() {
+    
   
   const column = [
 
     { field: 'id', headerName: 'Sl.No', walsIDth: 20, headerClassName: 'abc', },
-    { field: 'Review', headerName: 'ReviewStatus', walsIDth: 100, headerClassName: 'abc' },
+    {
+        field: 'actions',
+        type: 'actions',
+        headerName: 'Review',
+        sortable: false,
+        walsIDth: 50,
+        headerClassName: 'abc',
+        cellClassName: 'actions',
+        
+        renderCell: () => {
+          return (
+            <>
+           
+              <GridActionsCellItem
+              className="textPrimary"
+              color="primary"
+              icon={<EditIcon  />}
+              onClick={handleClickOpen}          
+            />
+              
+      
+             
+            </>
+          );
+        },
+      },
     { field: 'alsID', headerName: 'ALS ID', walsIDth: 50, headerClassName: 'abc' },
- 
+
     { field: 'client', headerName: 'Client', width: 150, headerClassName: 'abc', editable: "true" },
     { field: 'candidateName', headerName: 'Candidate name', width: 180, headerClassName: 'abc' },
     { field: 'openingBalance', headerName: 'Opening Bal', width: 120, headerClassName: 'abc' },
@@ -31,7 +68,15 @@ export default function DataTable() {
 
   ];
 
- 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   
   const [rows, setDataRow] = useState([]);
   const token = localStorage.getItem("token");
@@ -52,6 +97,35 @@ export default function DataTable() {
   return (
     <div style={{ height: 650, width: '100%' }} className="userList">
 
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Review</DialogTitle>
+        <DialogContent>
+         
+          <TextField
+            autoFocus
+            margin="dense"
+            id="candidateName"
+            label="Candidate Name"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+            <TextField 
+              autoFocus
+              margin="dense"
+        
+              label="Your Review"
+              type="text"
+              fullWidth
+              variant="standard"
+            
+            />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Send</Button>
+        </DialogActions>
+      </Dialog>
       <Box
         sx={{
           height: 650,
@@ -96,3 +170,4 @@ export default function DataTable() {
     </div>
   );
 }
+
